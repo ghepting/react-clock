17580f39635346252a31159c2a51b4fb1e1a5414
Gary Hepting <ghepting@gmail.com>
2020-03-16 13:23:29 -0700

    WIP
    - fixed second hand CSS transition bug
    – fractional minute/hour hand positioning
    - TODO: fix unknown bug with rapid cycle repeating on second hand

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
-import "./styles.css";
+import "./styles.scss";
 
 export default function App() {
   const getHandRotations = () => {
     const time = new Date();
     const secondsDecimal = time.getSeconds() / 60;
-    const minutesDecimal = time.getMinutes() / 60;
-    const hoursDecimal = (time.getHours() % 12) / 12;
+    const minutesDecimal = time.getMinutes() / 60 + secondsDecimal / 60;
+    const hoursDecimal = (time.getHours() % 12) / 12 + minutesDecimal / 60;
 
     return [hoursDecimal * 360, minutesDecimal * 360, secondsDecimal * 360];
   };
@@ -19,20 +19,36 @@ export default function App() {
     setTimeout(() => {
       let hour, minute, second;
       [hour, minute, second] = getHandRotations();
-      setClockHandRotations([hour, minute, second]);
+
+      let secondDiff = second - (clockHandRotations[2] % 360);
+      if (secondDiff < 0) secondDiff = 360 / 60; // from 360 back to zero
+      const newSeconds = clockHandRotations[2] + secondDiff;
+      console.log(secondDiff, clockHandRotations[2], second);
+
+      setClockHandRotations([hour, minute, newSeconds]);
     }, 100);
   });
 
   const getComputedStyles = index => {
     return {
-      transform: `scale(-1) rotate(${clockHandRotations[index]}deg)`
+      transform: `translateX(-50%) scale(-1) rotate(${
+        clockHandRotations[index]
+      }deg)`
     };
+  };
+
+  const generateListItems = n => {
+    return [...Array(n)].map((e, i) => {
+      return <li key={i} />;
+    });
   };
 
   const result = React.useMemo(() => {
     return (
       <div className="App">
         <div className="clock">
+          <ul className="lines">{generateListItems(60)}</ul>
+          <ol className="dial">{generateListItems(12)}</ol>
           <div className="hourHand" style={getComputedStyles(0)} />
           <div className="minuteHand" style={getComputedStyles(1)} />
           <div className="secondHand" style={getComputedStyles(2)} />
